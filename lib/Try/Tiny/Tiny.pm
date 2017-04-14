6a902b691b6744d85b867a632b68105a4f4d306c
Aristotle Pagaltzis <pagaltzis@gmx.de>
2017-04-14 17:33:13 +0200

    initial checkin

---
+++ lib/Try/Tiny/Tiny.pm
@@ -0,0 +1,54 @@
+package Try::Tiny::Tiny; use strict; use warnings;
+
+# ABSTRACT: slim Try::Tiny down as far as reasonably possible
+
+my $effective;
+
+BEGIN {
+	if ( not exists $INC{'Try/Tiny.pm'} ) {
+		my @hide = qw( Sub/Name.pm Sub/Util.pm );
+		local  @INC{ @hide };
+		delete @INC{ @hide };
+		local @INC = ( sub { die if 'Try::Tiny' eq caller and grep $_ eq $_[1], @hide }, @INC );
+		require Try::Tiny;
+	}
+	my $cb = sub { (caller 0)[3] };
+	my $name = &$cb;
+	$effective = $name eq &Try::Tiny::try($cb);
+	warn __PACKAGE__ . " is ineffective (probably loaded too late)\n" unless $effective;
+}
+
+$effective;
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+ export PERL5OPT=-MTry::Tiny::Tiny
+
+=head1 DESCRIPTION
+
+The reason this module exists is that L<Try::Tiny> will try naming the blocks
+it is passed, if it can load a module for doing so. This takes meaningful time,
+is only useful in stack traces, and then may defeat code that tries to examine
+the stack to print a more helpful error message.
+
+Now, you yourself can avoid using L<Try::Tiny> in the first place, but you will
+almost inevitably still pull it in through your dependency chain, and it is not
+likely to be feasible to scrub it out.
+
+This module helps sanitise your dependency chain a bit without patching all of
+it by loading L<Try::Tiny> in such a way that it is forced to skip naming its
+callbacks. In order to be useful, you must use this module before using any
+other module that loads L<Try::Tiny>. In practice that means you probably want
+to load it as early as possible, such as through L<PERL5OPT|perlrun/PERL5OPT>.
+
+=head1 DIAGNOSTICS
+
+This module will fail to load if you load it too late to take effect.
+
+=head1 SEE ALSO
+
+L<Try::Catch> E<ndash> a stripped-down clone of L<Try::Tiny>
